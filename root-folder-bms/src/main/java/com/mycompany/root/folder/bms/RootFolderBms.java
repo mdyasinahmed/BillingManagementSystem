ae25993e8697c71618535a8a90c8abfcd8bd1d3b
mdyasinahmed <yasinahmed7875@gmail.com>
2023-11-25 22:50:16 +0600

    add software login and homepage

--- root-folder-bms/src/main/java/com/mycompany/root/folder/bms/RootFolderBms.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.root.folder.bms;
-
-
-public class RootFolderBms {
-//
-//    public static void main(String[] args) {
-//        System.out.println("Hello World!");
-//    }
-}
